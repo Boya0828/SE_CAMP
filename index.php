6e8fb0148d678283d388e61c0ae3906a4fea72cc
Boya0828 <kidohagai@gmail.com>
2023-12-24 21:30:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,32 @@
 <html>
-    <body>
-        <h1>Thisis PHP<?php echo 'hello world'; ?></h1>
-        <div>
-            <p>1</p>
-        </div>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <?php $multi_x = 2; ?>
+    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+
+    <table>
+        <tbody>
+            for(int i=1;i<=24;i++){
+            <tr>
+                <td>$multi_x</td>
+                <td>x</td>
+                <td>i</td>
+                <td>=</td>
+                <td>i*2</td>
+            </tr>
+            }
+        </tbody>
+    </table>
+    <style>
+        table {
+            width: 150px;
+            height: 265px;
+        }
+    </style>
+</body>
+
 </html>
